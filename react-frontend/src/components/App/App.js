b3823e8d7b7b64a0051092e1ef8c90dfef919530
Hanna <marmalademarmoset@gmail.com>
2024-02-19 11:30:27 -0800

    Add formatting and stying to Main.js, add begenning routing to App.js

--- react-frontend/src/components/App/App.js
+++ react-frontend/src/components/App/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import NavBar from './NavBar';
+import Main from './Main'
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [testMessage, setTestMessage] = useState("");
@@ -9,10 +11,23 @@ function App() {
 
 
   return (
-    <div className="App">
-      <NavBar/>
-      
-    </div>
+    <>
+      {/* <Helmet>
+        <meta charSet="utf-8" />
+        <title>Vine Voice</title>
+        <link rel="canonical" href="http://www.vinevoice.org" />
+      </Helmet> */}
+      <NavBar />
+
+      <Routes>
+    
+        <Route path="/main" element={<Main />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/users/new" element={<CreateAccount />} /> */}
+ 
+ 
+      </Routes>
+    </>
   );
 }
 
