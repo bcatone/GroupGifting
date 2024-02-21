5ddbd5c392fc9c469ddce8fdb3fde5eb382fa521
Hanna <marmalademarmoset@gmail.com>
2024-02-21 11:56:04 -0800

    Delete dogpics from images folder, change TestData to get images from url

--- react-frontend/src/components/App/ItemLookup.js
+++ react-frontend/src/components/App/ItemLookup.js
@@ -1,22 +1,54 @@
 import React from 'react'
 import LeftBar from './LeftBar'
-import { Grid, Card, CardContent, Typography, Container } from "@mui/material"
+import { Grid, Card, CardContent, Typography, Container, CardActionArea, CardMedia, CardActions, Button } from "@mui/material"
+import Data from './TestData.json'
+
+console.log("data", Data)
 
 
 const ItemLookup = () => {
   return (
-<>
-<Container maxWidth="1g">
-<Typography variant="h4" align='center' style={{marginTop:"50px"}}>
-    Item Lookup
-</Typography>
-<Grid container spacing={5} style={{marginTop: "20px"}}>
+    <>
+      <Container maxWidth="lg">
+        <Typography variant="h4" align="center" style={{ marginTop: "50px" }}>
+          Item Lookup
+        </Typography>
+        <Grid container spacing={5} style={{ marginTop: "20px" }}>
+          {Data.map((result, index) => (
+            <Grid item xs={12} sm={4} ms={4} key={index}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="250"
+                   
+                    image={result.img}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {result.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {result.description}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      style={{ marginTop: "15px" }}
+                    >
+                      {result.deadline}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions></CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
-</Grid>
-
-</Container>
-</>
-  )
-}
-
-export default ItemLookup
+export default ItemLookup;
