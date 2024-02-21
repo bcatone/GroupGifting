aa3e401c88f29726943adfbc148b862141bba03d
Hanna <marmalademarmoset@gmail.com>
2024-02-21 11:13:04 -0800

    Make sure new files (TestData.json and temporary images are added)

---
+++ react-frontend/src/components/App/ItemLookup.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import LeftBar from './LeftBar'
+import { Grid, Card, CardContent, Typography, Container } from "@mui/material"
+
+
+const ItemLookup = () => {
+  return (
+<>
+<Container maxWidth="1g">
+<Typography variant="h4" align='center' style={{marginTop:"50px"}}>
+    Item Lookup
+</Typography>
+<Grid container spacing={5} style={{marginTop: "20px"}}>
+
+</Grid>
+
+</Container>
+</>
+  )
+}
+
+export default ItemLookup
