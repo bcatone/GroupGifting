228cc4a92c685d5b9bbf5a96620b538d62d846d1
Hanna <marmalademarmoset@gmail.com>
2024-02-23 19:12:46 -0800

    Fix LeftBar and Rename to SideBar, add in SideBarLayout to some pages in App.js Routing

--- react-frontend/src/components/App/App.js
+++ react-frontend/src/components/App/App.js
@@ -1,43 +1,53 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import NavBar from './NavBar';
-import Main from './Main'
-import { Route, Routes } from "react-router-dom";
-import ItemLookup from './ItemLookup';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import NavBar from "./NavBar";
+import Main from "./Main";
+import { Route, Routes, Outlet } from "react-router-dom";
+import ItemLookup from "./ItemLookup";
+import Donation from "./Donation";
+import SideBar from "./SideBar";
+import Restricted from "./Restricted";
+import SideBarLayout from "./SideBarLayout";
 
 function App() {
   // const [testMessage, setTestMessage] = useState("");
-
 
 
   return (
     <>
       {/* <Helmet>
         <meta charSet="utf-8" />
-        <title>Vine Voice</title>
-        <link rel="canonical" href="http://www.vinevoice.org" />
+        <title>Group Gifting</title>
+        <link rel="canonical" href="" />
       </Helmet> */}
       <NavBar />
 
       {/* Added some routing for future pages: */}
 
+      {/* SideBar Routes */}
       <Routes>
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<SideBarLayout />}>
+          <Route index element={<Main />} />
+          {/* <Route path="/users/:username" element={<UserAccount />} /> */}
+          {/* <Route path="/users/connections" element={<Connections />} /> */}
+          <Route path="/donation-info" element={<Donation />} />
+          <Route path="/items/all" element={<ItemLookup/>} />
+          {/* <Route path="/items/:id" element={<Item />} /> */}
+
+          {/* Not sure whether this page will have a sidebar ⬇️ */}
+          {/* <Route path="/items/new" element={<GiveawayItems />} /> */}
+        </Route>
+
         {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/users/new" element={<CreateAccount />} />  */}
-        {/* <Route path="/donation-info" element={<Donation />} /> */}
-        {/* <Route path="/items/new" element={<GiveawayItems />} /> */}
-        <Route path="/items/all" element={<ItemLookup />} />
-        {/* <Route path="/items/:id" element={<Item />} /> */}
-        {/* <Route path="/users/connections" element={<Connections />} /> */}
-        {/* <Route path="/users/:username" element={<UserAccount />} /> */}
+        {/* <Route path="/users/new" element={<SignUp />} />  */}
         {/* <Route path="/users/:username/edit" element={<EditAccount />} /> */}
         {/* <Route path="/not-found" element={<NotFound />} /> */}
-        {/* <Route path="/restricted" element={<Restricted />} />  */}
+        <Route path="/restricted" element={<Restricted />} /> 
       </Routes>
     </>
   );
 }
 
 export default App;
+
--- react-frontend/src/components/App/ItemLookup.js
+++ react-frontend/src/components/App/ItemLookup.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import LeftBar from './LeftBar'
-import { Grid, Card, CardContent, Typography, Container, CardActionArea, CardMedia, CardActions, Button } from "@mui/material"
-import Data from './TestData.json'
+import React from "react";
+import SideBar from "./SideBar";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Container,
+  CardActionArea,
+  CardMedia,
+  CardActions,
+  Button,
+} from "@mui/material";
+import { Link } from "react-router-dom"; // Import Link component
+import Data from "./TestData.json";
 
-console.log("data", Data)
-
+console.log("data", Data);
 
 const ItemLookup = () => {
   return (
@@ -13,36 +23,61 @@ const ItemLookup = () => {
         <Typography variant="h4" align="center" style={{ marginTop: "50px" }}>
           Item Lookup
         </Typography>
+        <SideBar data={Data} />
         <Grid container spacing={5} style={{ marginTop: "20px" }}>
           {Data.map((result, index) => (
             <Grid item xs={12} sm={4} ms={4} key={index}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="250"
-                   
-                    image={result.img}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {result.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {result.description}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      style={{ marginTop: "15px" }}
-                    >
-                      {result.deadline}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions></CardActions>
-              </Card>
+              <Link to={`/items/${result.id}`}>
+                {" "}
+                {/* Fix Link syntax */}
+                <Card
+                  sx={{ maxWidth: 345 }}
+                  style={{ padding: "10px", marginBottom: "30px" }}
+                >
+                  <CardActionArea
+                    style={{ display: "flex", justifyContent: "center", flexDirection:"column" }}
+                  >
+                    <CardMedia
+                      component="img"
+                      style={{
+                        height: `${345}px`,
+                        width: `345px`,
+                        // borderRadius: "5px",
+                        objectFit: "cover",
+                        justifyContent: "center",
+                      }}
+                      image={result.img}
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        align="center"
+                      >
+                        {result.title}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        align="center"
+                      >
+                        {result.description}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        style={{ marginTop: "15px" }}
+                        align="center"
+                      >
+                        {result.deadline}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions></CardActions>
+                </Card>
+              </Link>
             </Grid>
           ))}
         </Grid>
